165cf36ff752887b781b0cc587ea00622779e1f6
Louise <loui59f1@stud.kea.dk>
2021-03-09 16:39:19 +0100

    Adding and hiding classes

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ const features = {
     solarfan: false
 };
 
-window.addEventListener("DOMContentLoaded", start);
+window.addEventListener("DOMContentLoaded", init);
 
-function start() {
-    console.log("start");
+function init() {
+    console.log("Initiate program");
     // register toggle-clicks
     document.querySelectorAll(".option").forEach(option => option.addEventListener("click", toggleOption));
 }
@@ -21,33 +21,40 @@ function toggleOption(event) {
     const target = event.currentTarget;
     const feature = target.dataset.feature;
 
-    // TODO: Toggle feature in "model"
+    // Toggle feature in "model"
+    features[feature] = !features[feature];
 
-    // If feature is (now) turned on:
-    // - mark target as chosen (add class "chosen")
-    // - un-hide the feature-layer(s) in the #product-preview;
-    // - create featureElement and append to #selected ul
+    // If feature is (now) turned on: 
+    // - mark target as chosen (add class "chosen")      ✓
+    // - un-hide the feature-layer(s) in the #product-preview;       ✓
+    // - create featureElement and append to #selected ul ..... ??
     // - create FLIP-animation to animate featureElement from img in target, to
-    //   its intended position. Do it with normal animation or transition class!
+    //   its intended position. Do it with normal animation or transition class! .... ???
 
     // Else - if the feature (became) turned off:
-    // - no longer mark target as chosen
-    // - hide the feature-layer(s) in the #product-preview
+    // - no longer mark target as chosen       ✓
+    // - hide the feature-layer(s) in the #product-preview       ✓
     // - find the existing featureElement in #selected ul
     // - create FLIP-animation to animate featureElement to img in target
     // - when animation is complete, remove featureElement from the DOM
 
     if (features[feature]) {
-        // feature added
+        // Feature is added
         console.log(`Feature ${feature} is turned on!`);
 
-        // TODO: More code
+        // Add chosen class
+        target.classList.add("chosen");
+
+        document.querySelector(`[data-feature="${feature}"]`).classList.remove("hide");
 
     } else {
-        // feature removed
+        // Feature is removed
         console.log(`Feature ${feature} is turned off!`);
 
-        // TODO: More code
+        // Remove chosen class
+        target.classList.remove("chosen");
+
+        document.querySelector(`[data-feature="${feature}"]`).classList.add("hide");
 
     }
 }
